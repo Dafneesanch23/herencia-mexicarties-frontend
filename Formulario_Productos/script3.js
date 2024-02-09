82daa91a190a119e9cd6b619967981da741f8b76
Dafneesanch23 <daaphsanchez@gmail.com>
2024-02-09 12:24:55 -0600

    Se añaden los links de navegacion para CONTACTO y FORMULARIO DE CONTACTO y se hacen cambios en el script del FORMULARIO PRODUCTOS

---
+++ Formulario_Productos/script3.js
@@ -0,0 +1,47 @@
+// const fs = require('fs');
+
+// const producto = {
+//     idProducto: Math.floor(Math.random() * 1000),
+//     idUsuario: req.user.id, //Tendría que estar autenticado
+//     nombre: req.body['Nombre de producto'],
+//     categoria: req.body['categoria'],
+//     descripcion: req.body['descripcion'],
+//     artesano: req.body['artesano'],
+//     precio: req.body['precio'],
+//     lugarFabricacion: req.body['lugar']
+// };
+
+// const input = document.getElementById('nombre-producto');
+// input.addEventListener('input', (e) => {
+//     //console.log(e);
+//     console.log(e.target.value);
+// });
+
+// const csvContent = 'idProducto,idUsuario,nombre,tipo,descripcion,artesano,precio,lugarFabricacion\n' +
+//     productos.map(producto =>
+//         `${producto.idProducto},${producto.idUsuario},"${producto.nombre}","${producto.tipo}","${producto.descripcion}","${producto.artesano}",${producto.precio},"${producto.lugarFabricacion}"`
+//     ).join('\n');
+
+// fs.writeFileSync('productos.csv', csvContent, 'utf-8');
+
+
+
+function save(){
+    let producto = {
+        nombre:document.getElementById("nombre-producto").value,
+        descripcion:document.getElementById("descripcion-producto").value,
+        categoria:document.getElementById("categoria").value,
+        artesano:document.getElementById("artesano").value,
+        lugar:document.getElementById("lugar").value,
+        precio:document.getElementById("precio").value
+    };
+
+    // for (let key in producto) {
+    //     if (producto[key] === "") {
+    //         alert("Campos obligatorio");
+    //         return; 
+    //     }
+    // }
+
+    console.log(producto);
+}
