9cef885d8d722857456c67111a89dc690dd9d74d
susanynz <susanynz>
2024-02-13 14:07:50 -0600

    links y organizacion de carpetas

---
+++ DescubreMéxico/JS/descubreMexico.js
@@ -0,0 +1,13 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const cardInners = document.querySelectorAll(".card-inner");
+
+    cardInners.forEach(function(cardInner) {
+      cardInner.addEventListener("mouseenter", function() {
+        cardInner.classList.add("rotate");
+      });
+
+      cardInner.addEventListener("mouseleave", function() {
+        cardInner.classList.remove("rotate");
+      });
+    });
+  });
