1d0686297085906606c25d1c67ee0d83ba09aff1
susanynz <susanynz>
2024-02-20 10:58:21 -0600

    tienda ropa

--- Tienda/tiendaRopa/JS/tiendaRopa.js
+++ Tienda/tiendaRopa/JS/tiendaRopa.js
@@ -1,139 +1,152 @@
-/*Productos de tienda*/
-let productosMostrados = 0;
-const productosPorPagina = 15;
+// Datos de ejemplo de ropa mexicana
+const ropaArtesanal = [
+  {
+    name: 'Blusa bordada chedrón',
+    img: '/Inicio/images/blusabordada.png',
+    description: '',
+    price: 750,
+  },
+  {
+    name: 'Blusa bordada blanca',
+    img: './images/blusabordada2.png',
+    description: '',
+    price: 800,
+  },
+  {
+    name: 'Blusa bordada cuello V',
+    img: './images/blusabordada3.png',
+    description: '',
+    price: 680,
+  },
+  {
+    name: 'Huipil bordado',
+    img: './images/huipil.png',
+    description: '',
+    price: 870,
+  },
+  {
+    name: 'Vestido bordado',
+    img: './images/huipildama.png',
+    description: '',
+    price: 875,
+  },
+  {
+    name: 'Vestido bordado juvenil',
+    img: './images/vestidobordado1.png',
+    description: '',
+    price: 858,
+  },
+  {
+    name: 'Camisa bordada infantil',
+    img: './images/camisaniño.png',
+    description: '',
+    price: 680,
+  },
+  {
+    name: 'Huipil infantil',
+    img: './images/huipilniña.png',
+    description: '',
+    price: 650,
+  },
+  {
+    name: 'Camisa infantil bordada con botones',
+    img: './images/camisaniño2.png',
+    description: '',
+    price: 650,
+  },
+  {
+    name: 'Camisa bordada',
+    img: './images/camisahombre.png',
+    description: '',
+    price: 765,
+  },
+  {
+    name: 'Camisa bordada',
+    img: './images/camisahombre3.png',
+    description: '',
+    price: 765,
+  },
+  {
+    name: 'Camisa bordada',
+    img: './images/camisahombre2.png',
+    description: '',
+    price: 765,
+  }
 
-async function cargarProductos() {
-    const response = await fetch('/tienda_en_linea/javascript/productos.csv');
-    const csvData = await response.text();
+    // Agrega más ropa
+  ];
+  
+  // Función para mostrar la ropa artesanal mexicana en miniaturas
+  function mostrarMiniaturasRopaArtesanal() {
+    const grid = document.getElementById('liquor-grid');
+  
+    ropaArtesanal.forEach((ropa, index) => {
+      const productThumbnail = document.createElement('div');
+      productThumbnail.classList.add('product-thumbnail');
+  
+      // Crear la imagen
+      const image = document.createElement('img');
+      image.src = ropa.img;
+      image.alt = ropa.name;
+      image.addEventListener('click', () => mostrarDetallesProducto(licor));
+  
+      // Crear el texto "Ver más"
+      const overlayText = document.createElement('div');
+      overlayText.classList.add('overlay-text');
+      overlayText.textContent = 'Ver más';
+  
+      // Agregar evento click al texto "Ver más"
+      overlayText.addEventListener('click', () => mostrarDetallesProducto(ropa));
+  
+      // Agregar la imagen y el texto "Ver más" al thumbnail del producto
+      productThumbnail.appendChild(image);
+      productThumbnail.appendChild(overlayText);
+  
+      grid.appendChild(productThumbnail);
+    });
+  }
+  
 
-    const productos = parseCSV(csvData);
-
-    mostrarProductos(productos);
-}
-
-function parseCSV(csvData) {
-    const lines = csvData.split('\n');
-    const headers = lines[0].split(',');
-
-    const productos = [];
-
-    for (let i = 1; i < lines.length; i++) {
-        const currentLine = lines[i].split(',');
-        const producto = {};
-
-        for (let j = 0; j < headers.length; j++) {
-            producto[headers[j]] = currentLine[j];
-        }
-
-        productos.push(producto);
-    }
-
-    return productos;
-}
-
-//filtrar productos
-async function filtrarProductos() {
-    const searchBar = document.getElementById('searchBar');
-    const filtro = searchBar.value.toLowerCase();
-    const response = await fetch('/tienda_en_linea/javascript/productos.csv');
-    const csvData = await response.text();
-    const productos = parseCSV(csvData);
-
-    const productosFiltrados = productos.filter(producto =>
-        producto.lugar_fabricacion.toLowerCase().includes(filtro) ||
-        producto.tipo.toLowerCase().includes(filtro) ||
-        producto.artesano.toLowerCase().includes(filtro)
-    );
-
-    mostrarProductos(productosFiltrados);
-}
-
-//mostrar productos
-function mostrarProductos(productosAMostrar) {
-    const productosContainer = document.getElementById('productosContainer');
-    productosContainer.innerHTML = '';
-    productosMostrados = 0;
-
-    productosAMostrar.slice(0, productosPorPagina).forEach(producto => {
-        const productoCard = `
-    <div class="col-md-3 producto dark-card">
-        <div class="card option_container">
-            <div class="options">
-                <a class="option1" style="text-decoration: none;" onclick="echarAlHuacal()">
-                    Echar al huacal
-                </a>
-                <a href="" class="option2" style="text-decoration: none;">
-                    Comprar
-                </a>
-            </div>
-            <img src="${producto.imagen_url}" class="card-img-top" alt="${producto.imagen_alt}">
-            <div class="card-body">
-                <h5 class="card-title">${producto.nombre}</h5>
-                <p class="card-text">Precio: ${producto.precio} USD</p>
-            </div>
-        </div>
-    </div>
+  // Función para mostrar los detalles del producto en el modal
+  function mostrarDetallesProducto(producto) {
+    const modalBody = document.getElementById('productModalBody');
+    modalBody.innerHTML = `
+      <h2>${producto.name}</h2>
+      <img src="${producto.img}" alt="${producto.name}">
+      <p>${producto.description}</p>
+      <p>Precio: $${producto.price}</p>
+      <button class="btn btn-primary" onclick="echarAlHuacal('${producto.name}')">Echar al Huacal</button>
+      <button class="btn btn-secondary" onclick="agregarAListaDeDeseos('${producto.name}')">Agregar a Lista de Deseos</button>
     `;
+  
+    $('#productModal').modal('show');
+  }
+  
+  // Llama a la función para mostrar las miniaturas de los licores artesanales mexicanos
+  mostrarMiniaturasRopaArtesanal();
 
-        productosContainer.innerHTML += productoCard;
-        productosMostrados++;
-    });
-
-    const btnMostrarMas = document.querySelector('.btn-mostrar-mas');
-    btnMostrarMas.style.display = productosAMostrar.length > productosMostrados ? 'block' : 'none';
+  // Función para agregar un producto a la lista de deseos
+  function echarAlHuacal(nombreProducto) {
+    const producto = ropaArtesanal.find(ropa=> ropa.name === nombreProducto);
+    console.log('Producto añadido al huacal:', producto);
+    // Incrementar el contador de productos en el huacal
+    const huacalNumber = document.getElementById('huacal-number');
+    huacalNumber.textContent = parseInt(huacalNumber.textContent) + 1;
 }
 
-async function mostrarMasProductos() {
-    const productosContainer = document.getElementById('productosContainer');
-    const response = await fetch('/tienda_en_linea/javascript/productos.csv');
-    const csvData = await response.text();
-    const productos = parseCSV(csvData);
+// Obtener referencia al botón "Ver más"
+const verMasButton = document.getElementById('verMasButton');
 
-    productos.slice(productosMostrados, productosMostrados + productosPorPagina).forEach(producto => {
-        const productoCard = `
-        <div class="col-md-3 producto dark-card">
-        <div class="card option_container">
-            <div class="options">
-                <a class="option1" style="text-decoration: none;" onclick="echarAlHuacal()">
-                    Echar al huacal
-                </a>
-                <a href="" class="option2" style="text-decoration: none;">
-                    Comprar
-                </a>
-            </div>
-            <img src="${producto.imagen_url}" class="card-img-top" alt="${producto.imagen_alt}">
-            <div class="card-body">
-                <h5 class="card-title">${producto.nombre}</h5>
-                <p class="card-text">Precio: ${producto.precio} USD</p>
-            </div>
-        </div>
-    </div>
-    `;
+// Obtener referencia al modal
+const productModal = new bootstrap.Modal(document.getElementById('productModal'));
 
-        productosContainer.innerHTML += productoCard;
-        productosMostrados++;
-    });
-
-    const btnMostrarMas = document.querySelector('.btn-mostrar-mas');
-    btnMostrarMas.style.display = productos.length > productosMostrados ? 'block' : 'none';
-}
-
-function echarAlHuacal() {
-    var huacalNumber = document.getElementById('huacal-number');
-    var currentCount = parseInt(huacalNumber.innerText);
-    var newCount = currentCount + 1;
-    huacalNumber.innerText = newCount;
-}
-
-/* Script para el footer*/
-document.querySelector('.descripcion-section img').addEventListener('click', function () {
-    window.scrollTo({
-        top: 0,
-        behavior: 'smooth'
-    });
+// Agregar evento clic al botón "Ver más" para abrir el modal
+verMasButton.addEventListener('click', () => {
+    productModal.show(); // Mostrar el modal al hacer clic en el botón "Ver más"
 });
-
-/*Funciones a correr*/
-
-cargarProductos();
-load();
+  
+  // Función para agregar un producto a la lista de deseos
+  function agregarAListaDeDeseos(nombreProducto) {
+    const producto = ropaArtesanal.find(ropa => ropa.name === nombreProducto);
+    console.log('Producto añadido a la lista de deseos:', producto);
+  }
