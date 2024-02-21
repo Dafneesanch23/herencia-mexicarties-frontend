f5df197f9e3a5547c0819bf3421f511102fd2687
Juan-Gallegos7 <juangallegos.0397@gmail.com>
2024-02-21 13:32:42 -0600

    pagina descubre mexico2.0

---
+++ DescubreMéxico copy/JS/descubreMexico.js
@@ -0,0 +1,24 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const cardInners = document.querySelectorAll(".card-inner");
+
+    cardInners.forEach(function(cardInner) {
+      cardInner.addEventListener("mouseenter", function() {
+        cardInner.classList.add("rotate");
+      });
+
+      cardInner.addEventListener("mouseleave", function() {
+        cardInner.classList.remove("rotate");
+      });
+    });
+  });
+
+  // $(document).ready(function () {
+  
+
+/*tarjetas de estados*/
+$(document).ready(function () {
+  $('.card-back img').click(function () {
+    var cardId = $(this).data('card-id');
+    $('#modal' + cardId).modal('show');
+  });
+});
