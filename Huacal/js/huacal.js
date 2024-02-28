fb871730bfe3a8a8317228ad0d79282daa5ddec2
paconut <paconut1@gmail.com>
2024-02-27 22:19:09 -0600

    Se corrigió el que se agreguen los productos al local storage

--- Huacal/js/huacal.js
+++ Huacal/js/huacal.js
@@ -19,16 +19,15 @@ function mostrarProductosHuacal() {
                                 <p class="card-text">Artesano ${producto.artesano}</p>
                             </div>
                             <div class="quantity-selector d-flex flex-row align-items-center">
-                                <button class="btn btn-outline-secondary justify-content-center incrementar-disminuir"> - </button>
+                                <button class="btn btn-outline-secondary justify-content-center incrementar-disminuir" onclick="disminuirCantidad(this.nextElementSibling)"> - </button>
                                 <input type="text" value="1" class="form-control">
-                                <button class="btn btn-outline-secondary justify-content-center incrementar-disminuir"> + </button>
+                                <button class="btn btn-outline-secondary justify-content-center incrementar-disminuir" onclick="aumentarCantidad(this.previousElementSibling)"> + </button>
                             </div>
                             <p class="price mb-2 mb-sm-0">$${producto.precio} USD</p>
                         </div>
                         <div class="mt-auto">
                             <div class="d-flex justify-content-between mt-2">
-                                <button class="btn btn-opcion w-100 me-1 justify-content-center">Sacar del huacal</button>
-                                <button class="btn btn-opcion w-100 ms-1 justify-content-center">Guardar para después</button>
+                                <button class="btn btn-opcion w-100 me-1 justify-content-center" onclick="sacarDelHuacal(this)">Sacar del huacal</button>
                             </div>
                         </div>
                     </div>
@@ -40,11 +39,44 @@ function mostrarProductosHuacal() {
     });
 }
 
-window.onload = function () {
-    const huacalNumber = localStorage.getItem('huacalNumber');
-    if (huacalNumber !== null) {
-        document.getElementById('huacal-number').innerText = huacalNumber;
+function disminuirCantidad(input) {
+    var cantidad = parseInt(input.value);
+    if (cantidad > 1) {
+        cantidad--;
+        input.value = cantidad;
+        var cardBody = input.closest('.card-body');
+        var precio = parseFloat(cardBody.querySelector('.price').innerText.replace('$', '').replace(' USD', ''));
+        var cantidad = parseInt(input.value);
+        var total = precio / (cantidad + 1) * cantidad;
+        cardBody.querySelector('.price').innerText = '$' + total.toFixed(2) + ' USD';
     }
+}
+
+function aumentarCantidad(input) {
+    var cantidad = parseInt(input.value);
+    cantidad++;
+    input.value = cantidad;
+    var cardBody = input.closest('.card-body');
+    var precio = parseFloat(cardBody.querySelector('.price').innerText.replace('$', '').replace(' USD', ''));
+    var cantidad = parseInt(input.value);
+    var total = precio / (cantidad - 1) * cantidad;
+    cardBody.querySelector('.price').innerText = '$' + total.toFixed(2) + ' USD';
+}
+
+function sacarDelHuacal(button) {
+    var huacalNumber = document.getElementById('huacal-number');
+    var currentCount = parseInt(huacalNumber.innerText);
+    var newCount = currentCount - 1;
+    huacalNumber.innerText = newCount;
+
+    localStorage.setItem('huacalNumber', newCount);
+
+    var card = button.closest('.product-card');
+    var productName = card.querySelector('.card-title').innerText;
+    var storedProducts = JSON.parse(localStorage.getItem('storedProducts')) || [];
+    storedProducts = storedProducts.filter(product => product.nombre !== productName);
+    localStorage.setItem('storedProducts', JSON.stringify(storedProducts));
+    card.remove();
 }
 
 mostrarProductosHuacal()
